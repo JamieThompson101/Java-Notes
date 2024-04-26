2cea6e4ae00985ed955c1af47b194602a81eb738
Jamie Thompson <130515437+JamieThompson101@users.noreply.github.com>
2024-04-26 16:03:06 -0400

    new notes

--- practiceproblemsjava/main.java
+++ practiceproblemsjava/main.java
@@ -1,8 +1,9 @@
 /* the public class has to match the file name hence why it is class main */
-
+/* The main function starts as the entry point to your program: static method that is named main, has public access, takes a string as a parameter, and doesnt retrun any value  */
 package practiceproblemsjava;
 class Main {
     public static void main(String[] args) {
+        /* acts as print or console.log for java depending on what string value you pass through. Semi colons are mandatory in Java otherwise the program will fail to run */
         System.out.println("Hello World!");
     }
 }
